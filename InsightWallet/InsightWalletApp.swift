2b7cfb93a4d2c366fbb538fa0fed1f9b5af9e70e
TAIGA <st0731@users.noreply.github.com>
2026-04-15 21:35:10 +0900

    Initial Commit

---
+++ InsightWallet/InsightWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  InsightWalletApp.swift
+//  InsightWallet
+//
+//  Created by Taiga Sase on 2026/04/15.
+//
+
+import SwiftUI
+
+@main
+struct InsightWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
